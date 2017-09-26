fe05a5eeba77530459d9486978261a5475e8f9e0
Andrei Lespukh <alespukh@evolutiongaming.com>
2017-09-26 14:57:15 +0300

    * object test case

--- akka-protocol-serializer/src/test/scala/com/dosar/akka/protocol/serializer/ArrayProtocolSerializerSpec.scala
+++ akka-protocol-serializer/src/test/scala/com/dosar/akka/protocol/serializer/ArrayProtocolSerializerSpec.scala
@@ -11,12 +11,15 @@ class ArrayProtocolSerializerSpec extends WordSpec {
   "ArrayProtocolSerializer" should {
     ClazzRegistry.register[FailingCase]
     ClazzRegistry.register[RichCaseClass]
+    ClazzRegistry.register[Test.type]
 
     "work like to-array-serializer for failing case" in check(FailingCase(Nested(List(1, 2, 4))))
 
     "work like to-array-serializer for complex case class" in check {
       RichCaseClass(Some("hey!"), List(Bet("user", Some(10)), Bet("one more user", None)), 0.12, 1200l)
     }
+
+    "work like to-array-serializer for objects" in check(Test)
   }
 
   def check[T <: AnyRef: Serializer : Length](value: T) = {
@@ -35,3 +38,4 @@ case class Nested(a: List[Int])
 case class FailingCase(a: Nested)
 case class Bet(userId: String, amount: Option[Int])
 case class RichCaseClass(s: Option[String], bets: List[Bet], successRate: Double, allBetsCount: Long)
+object Test
